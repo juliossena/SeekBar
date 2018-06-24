9725014c7d982ec474b9563568750e6f7f0db22f
juliossena <juliosouzasena@gmail.com>
2018-06-23 21:18:35 -0300

    final

--- app/src/main/java/seekbar/cursoandroid/dell/seekbar/MainActivity.java
+++ app/src/main/java/seekbar/cursoandroid/dell/seekbar/MainActivity.java
@@ -1,13 +1,39 @@
 package seekbar.cursoandroid.dell.seekbar;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private SeekBar seekBar;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekBar_id);
+        resultado = (TextView) findViewById(R.id.text_resultado_id);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                resultado.setText("Progresso: " + i + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
